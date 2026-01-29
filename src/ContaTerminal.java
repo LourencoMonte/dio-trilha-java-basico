e9983d838a8d4a764a06ec6c5fa1c7571bf377be
Lourenco Monte <f.lourenco13aps@gmail.com>
2026-01-29 18:48:38 -0300

    feat: Adiciona criação de conta e boas vindas

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,25 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua agência:");
+        String agencia = sc.nextLine();
+
+        System.out.println("Digite o número da sua conta:");
+        String conta = sc.nextLine();
+
+        System.out.println("Digite seu nome:");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite o valor inicial para abertura de conta:");
+        float saldo = sc.nextFloat();
+        sc.nextLine(); 
+
+        System.out.printf("Olá %s!. Bem-vindo ao banco Master! Sua agencia é %s e sua conta %s. ", nome, agencia, conta);
+        System.out.printf("Seu saldo inicial é: R$ %.2f. ", saldo);
+
+        sc.close();
     }
 }
